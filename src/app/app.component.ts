e89dd33bf2b9a24d0c0e55fe01ab32fbc6d7a84c
Tanmay sharma <tanmay.sharma@appcino.com>
2023-11-21 19:16:07 +0530

    made new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [
+    HomeComponent,
+  ],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo2.png" alt="logo" aria-hidden="true">
+      <p>{{title}}</p>
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'testing and understanding';
 }
